60ef60d30b9b0b73f522933fbada07ab5f18f6f9
slawek <slawomirwojtowicz@gmail.com>
2020-05-03 13:52:28 +0200

    s

--- automated-tests-master/core/src/main/java/pl/ninebits/qa/automated/tests/core/tests/Condition.java
+++
@@ -1,5 +0,0 @@
-package pl.ninebits.qa.automated.tests.core.tests;
-
-public interface Condition {
-    boolean isFulfilled();
-}
--- automated-tests-master/testrail-client/src/main/java/com/rmn/testrail/parameters/ApiParameters.java
+++
@@ -1,8 +0,0 @@
-package com.rmn.testrail.parameters;
-
-public class ApiParameters {
-    public static String append(ApiParameter parameter, int value) {
-        return parameter.parameter + value;
-    }
-}
-
